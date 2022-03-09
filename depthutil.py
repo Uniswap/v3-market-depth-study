fixed depth calculation method 2 and added initial unit test

depthutil.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def genLiqRange(dft, tickwindow=60):
+def genLiqRange(dft, tickspacing=60):
     #' Generate Liquidity Distribution (liquidity amount) based on default tick size
+    #' returns dataframe with columns 'tickLower', 'amount', 'price'
     if not (
-        all(dft.tickLower % tickwindow == 0) & all(dft.tickUpper % tickwindow == 0)
+        all(dft.tickLower % tickspacing == 0) & all(dft.tickUpper % tickspacing == 0)
     ):
         print("fail tick window")
-    rgn = np.arange(dft.tickLower.min(), dft.tickUpper.max() + 1, tickwindow)
+    rgn = np.arange(dft.tickLower.min(), dft.tickUpper.max() + 1, tickspacing)
     dfrgn = pd.DataFrame({"tickLower": rgn, "amount": 0}).set_index("tickLower")
     for i, row in dft.iterrows():
-        dfrgn.loc[np.arange(row.tickLower, row.tickUpper, tickwindow), "amount"] = (
-            dfrgn.loc[np.arange(row.tickLower, row.tickUpper, tickwindow), "amount"]
+        dfrgn.loc[np.arange(row.tickLower, row.tickUpper, tickspacing), "amount"] = (
+            dfrgn.loc[np.arange(row.tickLower, row.tickUpper, tickspacing), "amount"]
             + row["amount"]
         )  # /(row['tickUpper']-row['tickLower'])
     dfrgn = dfrgn.reset_index()
@@ -22,88 +23,74 @@ def genLiqRange(dft, tickwindow=60):
     return dfrgn
 
 
-def genLiqRangeOverTime(df, savefile="", tickwindow=60):
+def genLiqRangeOverTime(df, savefile="", tickspacing=60):
     #' Generate multiple dates of liquidity range
     dfs = df.groupby(["date", "tickLower", "tickUpper"]).amount.sum()
     dft2 = pd.DataFrame(dfs).reset_index()
     rgns = dict()
     for dt in dft2["date"].unique():
-        rgns[dt] = genLiqRange(dft2.loc[dft2.date <= dt], tickwindow=tickwindow)
+        rgns[dt] = genLiqRange(dft2.loc[dft2.date <= dt], tickspacing=tickspacing)
     dfrgns = pd.concat(rgns).droplevel(1).rename_axis("date")
     if savefile != "":
         dfrgns.to_pickle("dfrgns.pkl")
     return dfrgns
 
 
-def calcdollarliq(dfrgn, tickwindow=60, alt=0):
-    # convert liquidity amount to dollar liquidity amount
+def genLiqRangeXNumeraire(dfrgn, tickspacing=60, alt=1):
+    #' convert liquidity amount to dollar liquidity amount
+    #' input dfrgn is a dataframe of liquidity distribution with columns 'tickLower', 'amount', 'P' as current price,  ['price' associated with tick]
+    #' returns dataframe with columns: 'tickLower', 'amount', 'price', 'P', 'pa', 'pb', 'p', 'x', 'y',
+    #' 'liqX', 'depth'
     dfrgn = dfrgn.assign(pa=lambda x: 1.0001 ** x.tickLower)
-    dfrgn = dfrgn.assign(pb=lambda x: 1.0001 ** (x.tickLower + tickwindow))
+    dfrgn = dfrgn.assign(pb=lambda x: 1.0001 ** (x.tickLower + tickspacing))
     # dfrgn=dfrgn.assign(x=lambda x: x.amount*(x.pb**0.5-x.pa**0.5)/((x.pa*x.pb)**0.5))
     dfrgn["p"] = 1 / dfrgn.P * 1e12
 
-    dfrgn.loc[dfrgn.p <= dfrgn.pa, "x"] = (
-        dfrgn.amount
-        * (dfrgn.pb ** 0.5 - dfrgn.pa ** 0.5)
-        / ((dfrgn.pa * dfrgn.pb) ** 0.5)
-        / 1e6
-    )
-    dfrgn.loc[(dfrgn.pa < dfrgn.p) & (dfrgn.p < dfrgn.pb), "x"] = (
-        dfrgn.amount
-        * (dfrgn.pb ** 0.5 - dfrgn.p ** 0.5)
-        / ((dfrgn.p * dfrgn.pb) ** 0.5)
-        / 1e6
-    )
+    dfrgn.loc[dfrgn.p <= dfrgn.pa, "x"] = ( dfrgn.amount * (dfrgn.pb ** 0.5 - dfrgn.pa ** 0.5) / ((dfrgn.pa * dfrgn.pb) ** 0.5) / 1e6 )
+    dfrgn.loc[(dfrgn.pa < dfrgn.p) & (dfrgn.p < dfrgn.pb), "x"] = ( dfrgn.amount * (dfrgn.pb ** 0.5 - dfrgn.p ** 0.5) / ((dfrgn.p * dfrgn.pb) ** 0.5) / 1e6 )
     dfrgn.loc[dfrgn.p >= dfrgn.pb, "x"] = 0
 
     dfrgn.loc[dfrgn.p <= dfrgn.pa, "y"] = 0
-    dfrgn.loc[(dfrgn.pa < dfrgn.p) & (dfrgn.p < dfrgn.pb), "y"] = (
-        dfrgn.amount * (dfrgn.p ** 0.5 - dfrgn.pa ** 0.5) / 1e18
-    )
-    dfrgn.loc[dfrgn.p >= dfrgn.pb, "y"] = (
-        dfrgn.amount * (dfrgn.pb ** 0.5 - dfrgn.pa ** 0.5) / 1e18
-    )
+    dfrgn.loc[(dfrgn.pa < dfrgn.p) & (dfrgn.p < dfrgn.pb), "y"] = ( dfrgn.amount * (dfrgn.p ** 0.5 - dfrgn.pa ** 0.5) / 1e18 )
+    dfrgn.loc[dfrgn.p >= dfrgn.pb, "y"] = ( dfrgn.amount * (dfrgn.pb ** 0.5 - dfrgn.pa ** 0.5) / 1e18 )
 
-    # convert liquidity using price at lower tick
-    dfrgn["liqusd"] = 1 * dfrgn.x + dfrgn.price * dfrgn.y
     if alt:
         # note that here P is the current price rather than the price at each tick
-        dfrgn["liqusd"] = 1 * dfrgn.x + dfrgn.P * dfrgn.y
+        dfrgn["liqX"] = 1 * dfrgn.x + dfrgn.P * dfrgn.y
+    else:
+        # convert liquidity using price at lower tick
+        dfrgn["liqX"] = 1 * dfrgn.x + dfrgn.price * dfrgn.y
     return dfrgn
 
 
-def calcDepth(dfrgn):
-    # Generate depth of any liquidity range/distribution by intergrating across the range
-    df1 = dfrgn.loc[dfrgn.price > dfrgn.P].sort_values("price")
-    df2 = dfrgn.loc[dfrgn.price <= dfrgn.P].sort_values("price")
-    df2["depth"] = df2.loc[::-1, "liqusd"].cumsum()[::-1]
-    df1 = df1.assign(depth=lambda x: x.liqusd.cumsum())
+def genDepth(dfrgn, tickspacing=60,alt=1):
+    #' Calculate dollar liquidity amount distribution and depth chart
+    dfrgnD = genLiqRangeXNumeraire(dfrgn, tickspacing=tickspacing,alt=alt)
+    #' Generate depth of any liquidity range/distribution by integrating across the range
+    df1 = dfrgnD.loc[dfrgnD.price > dfrgnD.P].sort_values("price")
+    df2 = dfrgnD.loc[dfrgnD.price <= dfrgnD.P].sort_values("price")
+    df2["depth"] = df2.loc[::-1, "liqX"].cumsum()[::-1]
+    df1 = df1.assign(depth=lambda x: x.liqX.cumsum())
     dfm = pd.concat([df2, df1])
-    return dfm
+
+    # depth=dfm.loc[(dfm.price>=dfm.P*0.97) & (dfm.price<=dfm.P*1.03)].liqX.sum()
+    return dfm#[["date", "tickLower", "price", "P", "liqX", "depth"]]
 
 
-def calc_usdliq_depth(dfrgn, tickwindow=60):
-    #' Calculate dollar liquidity distribution and depth chart
-    dfrgn = calcdollarliq(dfrgn, tickwindow=tickwindow)
-    dfm = calcDepth(dfrgn)
-    # depth=dfm.loc[(dfm.price>=dfm.P*0.97) & (dfm.price<=dfm.P*1.03)].liqusd.sum()
-    return dfm[["date", "tickLower", "price", "P", "liqusd", "depth"]]
-
-
-def genFullLiqDistribution(dfrgns, tickwindow=60):
-    #' Calls calc_usdliq_depth to getnerate full range of liquidity distribution, dollar liquidity and depth
+def genDepthOverTime(dfrgns, tickspacing=60):
+    #' Calls genDepth to getnerate full range of liquidity distribution, dollar liquidity and depth
     liqdist = list()
     for dt in dfrgns["date"].unique():
         liqdist.append(
-            calc_usdliq_depth(dfrgns.loc[dfrgns.date == dt], tickwindow=tickwindow)
+            genDepth(dfrgns.loc[dfrgns.date == dt], tickspacing=tickspacing)
         )
     #     pd.concat(liqdist).to_pickle('data/liqdist_eth.pkl')
     dfliqdist = pd.concat(liqdist)
     return dfliqdist
 
 
-def expandliqdistrib(df, depthpct=0.02, returnDepth=1, tickwindow=60):
-    # expand liquidity distribution granularity to 1 tick for a single date
+def fillGranularDistribution(df, depthpct=0.02, returnDepth=1, tickspacing=60):
+    # expand liquidity distribution granularity to 1 tick, effectively resampling tickspacing to 1 tick
     dft = df.copy()
     P = dft.P.values[0]
     if depthpct == 1:
@@ -115,30 +102,21 @@ def expandliqdistrib(df, depthpct=0.02, returnDepth=1, tickwindow=60):
         P_u = dft.P.values[0] * (1 - depthpct)
         P_l = dft.P.values[0] * (1 + depthpct)
         tick_u = (
-            int(np.log((1 / P_u * 1e12)) / np.log(1.0001) / tickwindow) + 1
-        ) * tickwindow
+            int(np.log((1 / P_u * 1e12)) / np.log(1.0001) / tickspacing) + 1
+        ) * tickspacing
         tick_l = (
-            int(np.log((1 / P_l * 1e12)) / np.log(1.0001) / tickwindow) - 1
-        ) * tickwindow
+            int(np.log((1 / P_l * 1e12)) / np.log(1.0001) / tickspacing) - 1
+        ) * tickspacing
 
-    dft["liqusdpertick"] = dft["liqusd"] / tickwindow
+    dft["liqXpertick"] = dft["liqX"] / tickspacing
     dft["tick_u"] = tick_u
     dft["tick_l"] = tick_l
 
     rgn = np.arange(tick_l, tick_u + 1, 1)
     dfrgn = pd.DataFrame({"tickLower": rgn, "depthattick": 0})
     dfrgn["price"] = 1e12 / (1.0001 ** dfrgn.tickLower)
-    for i, row in dft[
-        (
-            (dft.tickLower >= dft.tick_l - tickwindow)
-            & (dft.tickLower <= dft.tick_u + tickwindow)
-        )
-    ].iterrows():
-        dfrgn.loc[
-            (dfrgn.tickLower >= row.tickLower)
-            & (dfrgn.tickLower <= row.tickLower + tickwindow),
-            "depthattick",
-        ] = row["liqusdpertick"]
+    for i, row in dft[ ( (dft.tickLower >= dft.tick_l - tickspacing) & (dft.tickLower <= dft.tick_u + tickspacing) ) ].iterrows():
+        dfrgn.loc[ (dfrgn.tickLower >= row.tickLower) & (dfrgn.tickLower <= row.tickLower + tickspacing), "depthattick", ] = row["liqXpertick"]
     depth = dfrgn[(dfrgn.price >= P_u) & (dfrgn.price <= P_l)].depthattick.sum()
     depthbid = dfrgn[(dfrgn.price >= P_u) & (dfrgn.price <= P)].depthattick.sum()
     depthask = dfrgn[(dfrgn.price >= P) & (dfrgn.price <= P_l)].depthattick.sum()
@@ -148,16 +126,16 @@ def expandliqdistrib(df, depthpct=0.02, returnDepth=1, tickwindow=60):
         return dfrgn[(dfrgn.price >= P_u) & (dfrgn.price <= P_l)]
 
 
-def genDepthTS(liqdist, depthpct=0.02, tickwindow=60):
-    # Generate depth time series by calling expandliqdistrib for all dates
+def fillGranularDistributionOverTime(liqdist, depthpct=0.02, tickspacing=60):
+    # Generate depth time series by calling fillGranularDistribution for all dates
     depths = list()
     for dt in liqdist.date.unique():
         depths.append(
-            expandliqdistrib(
+            fillGranularDistribution(
                 liqdist.loc[liqdist.date == dt],
                 depthpct=depthpct,
                 returnDepth=1,
-                tickwindow=tickwindow,
+                tickspacing=tickspacing,
             )
         )
     dfdepth = pd.DataFrame(depths, index=pd.to_datetime(liqdist.date.unique())).rename(
@@ -173,28 +151,40 @@ def genDepthTS(liqdist, depthpct=0.02, tickwindow=60):
 def CalcAll(
     filein="../data/mintburn_usdceth3000_simple.csv",
     fileprice="../data/ethprice.csv",
-    tickwindow=60,
+    tickspacing=60,
     depthpct=0.02,
+    address='0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8',
+    bigquery=1
 ):
     dfprice = pd.read_csv(fileprice).rename(columns={"price": "P"})
     dfprice["date"] = pd.to_datetime(dfprice.date).dt.date
 
-    df = pd.read_csv(filein)
+    if(bigquery):
+        # read data from bigquery, a slightly different format than dune
+        df = pd.read_csv(filein, dtype={'amount': float,
+                         'amount0': float, 'amount1': float})
+        df = df.loc[df.address == address]
+        df = df.rename(columns={"lowertick": "tickLower",
+                       "uppertick": "tickUpper", "block_timestamp": "call_block_time"})
+    else:#dune
+        df = pd.read_csv(filein)
+
+    # conversions
     df["amount"] = df.amount.map(float)
     df = df.rename(columns={"lowertick": "tickLower", "uppertick": "tickUpper"})
     df["date"] = pd.to_datetime(df.call_block_time).dt.date
 
     # default liq distribution
-    dfrgns = genLiqRangeOverTime(df, savefile="", tickwindow=tickwindow)
-    # merge in price and adhoc filter
+    dfrgns = genLiqRangeOverTime(df, savefile="", tickspacing=tickspacing)
+    # merge in price
     dfrgns = dfrgns.reset_index()
     dfrgns["date"] = pd.to_datetime(dfrgns.date)
     dfprice["date"] = pd.to_datetime(dfprice.date)
-
     dfrgns = pd.merge(dfrgns, dfprice, on="date")
-    # dfrgns=dfrgns.loc[dfrgns.price<6000]
-    # Calc usd liquidity
-    liqdist = pd.DataFrame(genFullLiqDistribution(dfrgns, tickwindow=tickwindow))
-    # generate 1-tick distribution and depth
-    dfdepth = genDepthTS(liqdist, depthpct=depthpct, tickwindow=tickwindow)
+
+    # calc depth distribution based on default tick spacing
+    liqdist = pd.DataFrame(genDepthOverTime(dfrgns, tickspacing=tickspacing))
+
+    # expand into granular distribution and depth
+    dfdepth = fillGranularDistributionOverTime(liqdist, depthpct=depthpct, tickspacing=tickspacing)
     return dfdepth, liqdist, dfrgns

depthutil2.py
@@ -99,7 +99,7 @@ def calc_liquidity_at_tick(i,din, s=60, p=np.nan):
     li=lambjs/s
     return li
 
-def calc_market_depth(df,i0=198120,delta=0.02,ts=60,plusminus=True,logdelta=False,diagnosis=False,decimals0=1e6):
+def calc_market_depth(df,i0=198120,delta=0.02,ts=60,plusminus=True,logdelta=False,diagnosis=False,decimals0=6):
     # ' returns market depth given a dataframe with columns 'tickLower', 'amount' and current tick i0,
     # df has a mapping between tick and liqudity in raw form and current price p
     if logdelta:
@@ -115,17 +115,18 @@ def calc_market_depth(df,i0=198120,delta=0.02,ts=60,plusminus=True,logdelta=Fals
     # dataframe to dict and price to scalar for faster processing
     din=dict(df.set_index('tickLower').amount)
     # subset to list of ticks that have values
-    r=np.array([int(i) for i in r if i in din.keys()])
+    # r=np.array([int(i) for i in r if i in din.keys()])
 
 #     p=df.p.values[0]
     p=1.0001**i0
     if(diagnosis):
         # returns liquidity at each tick spacing
         lis=list()
+        print('d',d)
         print('min',min(r))
         print('max',max(r))
         for i in r:
-            lis.append(calc_liquidity_at_tick(i,din=din,s=ts,p=p)/decimals0)
+            lis.append(calc_liquidity_at_tick(i,din=din,s=ts,p=p)/(10**decimals0))
         m=sum(lis)
         print(d*2)
         print(len(lis))
@@ -135,9 +136,9 @@ def calc_market_depth(df,i0=198120,delta=0.02,ts=60,plusminus=True,logdelta=Fals
         m=0
         for i in r:
             m+=calc_liquidity_at_tick(i,din=din,s=ts,p=p)
-        return m/decimals0
+        return m/(10**(decimals0))
 
-def genMarketDepth(dfm,delta=0.02,ts=60,plusminus=True,logdelta=True,decimals0=1e6):
+def genMarketDepth(dfm,delta=0.02,ts=60,plusminus=True,logdelta=True,decimals0=6):
     # generate time series of market depth for specific delta
     # dfm contains columns "block_number", 'tickLower', 'amount', 'currenttick' (for tick associated with current price)
     blockN=dfm.block_number.unique()

example.py
@@ -96,7 +96,35 @@ dfm2=dpu2.genLiqRangeXNumeraire(dfm,tickspacing=ts,decimals0=decimals0,decimals1
 liqsumbydate=dfm2.groupby(['date','block_number']).agg({'x':sum,'y':sum,'liqX':sum,'amount':sum})
 
 bn=liqsumbydate.reset_index().block_number.max(); bn
-liqsumbydate.tail(5)
+
+dfm2.loc[dfm2.block_number==14260474].sum().amount
+
+
+int(np.floor(197484/60)*60)
+dfm2.loc[(dfm2.block_number==14260474) & (dfm2.tickLower==int(np.floor(197484/60)*60))]
+
+reload(dpu2)
+tmpdf=dfm2.loc[(dfm2.block_number==14260474) & (dfm2.tickLower==int(np.floor(197484/60)*60))]
+tmpdf.p.values[0]
+1/2653.3792
+1.0001**197484
+dpu2.calc_liquidity_at_tick(i=197460,din=tmpdf,p=1.0001**197484)/1e6
+
+i0=197484
+delta=-0.00242
+d=int(np.log((delta+1)*1.0001**i0)/(np.log(1.0001))-i0)
+d
+
+np.arange(i0,i0+d,np.sign(d))
+
+dpu2.calc_market_depth(df=dfm2.loc[(dfm2.block_number==14260474)],i0=197484,delta=-0.002397002598245934,plusminus=False,logdelta=False,diagnosis=True)
+dpu2.calc_market_depth(df=dfm2.loc[(dfm2.block_number==14260474)],i0=197484,delta=0.00360630714589405,plusminus=False,logdelta=False,diagnosis=True)
+
+1057296.5413859035+725003.3426646198
+24+35
+60*30208.233333333334
+
+
 import subgraph
 reload(subgraph)
 